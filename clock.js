5cccc7457738ddad7777f692543c4e31c15d22bd
Igor <gara314@gmail.com>
2017-06-11 21:46:18 +0300

    CronTab

--- clock.js
+++ clock.js
@@ -1,12 +1,10 @@
 var CronJob = require('cron').CronJob;
-
-function test() {
-    console.log('clock test')
-}
+var task = require('./task');
+const cronTime = process.env.CRON_TIME;
 
 new CronJob({
-  cronTime: "15 * * * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
-  onTick: test,
+  cronTime: cronTime,
+  onTick: task,
   start: true,
   timeZone: "Europe/Kiev"
 });
--- task.js
+++ task.js
@@ -2,22 +2,33 @@
 var winston = require('winston'),
     request = require('request'),
     bot = require('./telegram/bot'),
-    connectionService = require('./services/connectionService'),
     url = 'https://igov.org.ua/api/service/flow/63?nID_Server=0&nID_Service=176&nID_SubjectOrganDepartment=18&sID_Public_SubjectOrganJoin=1375'
 
 winston.level = process.env.DEBUG_LEVEL || 'info';
 
-request.get(url, function (error, response, body) {
-    if (!error) {
-        console.log(JSON.stringify(body));
-        let json = JSON.parse(body);
-        if (json.aDay.length > 0) {
-            bot.notifyAll('vacant time');
+var vacantNotify = true;
+
+module.exports = function() {
+    winston.info('fetch data');
+    request.get(url, function (error, response, body) {
+        if (!error) {
+            console.log(JSON.stringify(body));
+            let json = JSON.parse(body);
+            if (json.aDay.length > 0) {
+                if (vacantNotify) {
+                    bot.notifyAll('vacant time').then(() => {
+                        vacantNotify = false;
+                    });
+                }
+            }
+            else if (!vacantNotify) {                
+                bot.notifyAll('no vacant time').then(() => {
+                    vacantNotify = true;
+                });
+            }
         }
-        bot.stopPolling();
-        connectionService.closeConnection();
-    }
-    else {
-        winston.error('request error: ', error);
-    }
-});
+        else {
+            winston.error('request error: ', error);
+        }
+    });
+}
